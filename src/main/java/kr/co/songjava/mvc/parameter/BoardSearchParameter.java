0c29e257d27f8b81b260bb0359afd2755a456248
LEE HYO JEONG <hyojeong1913@naver.com>
2021-05-05 22:12:30 +0900

    배열 검색 기능

--- src/main/java/kr/co/songjava/mvc/parameter/BoardSearchParameter.java
+++ src/main/java/kr/co/songjava/mvc/parameter/BoardSearchParameter.java
@@ -1,6 +1,9 @@
 package kr.co.songjava.mvc.parameter;
 
+import kr.co.songjava.mvc.domain.BoardType;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * 게시글 검색 파라미터
@@ -9,6 +12,7 @@ import lombok.Data;
 public class BoardSearchParameter {
 
     private String keyword;
+    private List<BoardType> boardTypes;
 
     // 기본 생성자
     public BoardSearchParameter() {
