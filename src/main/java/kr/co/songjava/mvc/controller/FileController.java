73d977bc2e1cc360e540d9648b9631c86a984b32
LEE HYO JEONG <hyojeong1913@naver.com>
2021-05-10 23:57:49 +0900

    파일 업로드 구현

--- src/main/java/kr/co/songjava/mvc/controller/FileController.java
+++ src/main/java/kr/co/songjava/mvc/controller/FileController.java
@@ -3,13 +3,16 @@ package kr.co.songjava.mvc.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import kr.co.songjava.configuration.GlobalConfig;
+import kr.co.songjava.configuration.exception.BaseException;
 import kr.co.songjava.configuration.http.BaseResponse;
+import kr.co.songjava.configuration.http.BaseResponseCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Calendar;
 
 @RestController
 @RequestMapping("/file")
@@ -26,12 +29,22 @@ public class FileController {
      *
      * @return
      */
-    @GetMapping
+    @PostMapping("/save")
     @ApiOperation(value = "업로드", notes = "")
-    public BaseResponse<Boolean> save() {
-        logger.debug("config : {}", config);
+    public BaseResponse<Boolean> save(@RequestParam("uploadFile") MultipartFile multipartFile) {
+
+        logger.debug("multipartFile : {}", multipartFile);
+
+        if (multipartFile == null || multipartFile.isEmpty()) {
+            throw new BaseException(BaseResponseCode.DATA_IS_NULL);
+        }
+
         String uploadFilePath = config.getUploadFilePath();
         logger.debug("uploadFilePath : {}", uploadFilePath);
+
+        if (config.isProd()) {
+            logger.debug("isProd calendar : {}", Calendar.getInstance());
+        }
 
         return new BaseResponse<Boolean>(true);
     }
